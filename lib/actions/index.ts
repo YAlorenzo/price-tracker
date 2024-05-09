240533f166d598010c66e17855b9a39b8bd3c793
YAlorenzo <zhenia.klimenchuk@gmail.com>
2024-05-09 21:20:48 +0200

    searchbar

---
+++ lib/actions/index.ts
@@ -0,0 +1,16 @@
+"use server"
+
+import { scrapeAmazonProduct } from "../scraper";
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+    if (!productUrl) return;
+
+    try {
+        const scrapedProduct = await scrapeAmazonProduct(productUrl);
+        
+    } catch (error: any) {
+        throw new Error(`Faild to create/update product: ${error.message}`)
+    }
+
+    
+}
