db778fb1d9d719ff68e94a22f616f0d18adf8164
YAlorenzo <zhenia.klimenchuk@gmail.com>
2024-05-11 18:31:29 +0200

    addConnectDataBase&&fetchData

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,16 +1,62 @@
-"use server"
+"use server";
 
+import { revalidatePath } from "next/cache";  
+import Product from "../models/product.model";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
-    if (!productUrl) return;
+  if (!productUrl) return;
 
-    try {
-        const scrapedProduct = await scrapeAmazonProduct(productUrl);
-        
-    } catch (error: any) {
-        throw new Error(`Faild to create/update product: ${error.message}`)
+  try {
+    connectToDB();
+
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+    if (!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
     }
 
-    
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+      );
+      revalidatePath(`/product/${newProduct._id}`);
+      
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`);
+  }
+}
+
+
+export async function getProductById(productId: string) {
+    try {
+        connectToDB();
+
+        const product = await Product.findOne({ _id: productId });
+
+        if (!product) return null;
+
+        return product;
+    } catch (error) {
+        console.log(error);
+    }
 }
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+import { extractCurrency, extractDescription, extractDiscountRate, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -39,10 +39,54 @@ export async function scrapeAmazonProduct(url: string) {
 
     const originalPrice = extractPrice(
       $("#priceblock_ourprice"),
-      $(".a-price.a-text-price span.a-offscreen")
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listP"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
     );
 
-    console.log({ title, currentPrice });
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      '{}'    
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    const currency = extractCurrency($('.a-price-symbol'));
+    const discountRate = extractDiscountRate($(".savingsPercentage"));
+
+    const description = extractDescription($)
+
+    console.log({ title, currentPrice, originalPrice, outOfStock,  imageUrls, currency, discountRate});
+
+    //dataObj with scraped info of product
+
+    const data = {
+      url,
+      currency: currency || '$',
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: 'category',
+      reviewsCount: 100,
+      stars: 4.5,
+      isOutOfStock: outOfStock,
+      description,
+      lowerPrice: Number(currentPrice) || Number(originalPrice), 
+      highesPrice: Number(originalPrice) || Number(currentPrice),
+      average: Number(currentPrice) || Number(originalPrice),
+    }
+
+    // console.log(data);
+
+    return data;
+
   } catch (error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`);
   }
