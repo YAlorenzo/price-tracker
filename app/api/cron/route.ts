0e804baadb12c1e40e8571c2dbe51633b1cafb63
YAlorenzo <zhenia.klimenchuk@gmail.com>
2024-05-20 17:33:46 +0200

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,8 +6,8 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { NextResponse } from "next/server";
 
 // export const maxDuration = 60;
-// export const dynamic = 'force-dynamic';
-// export const revalidate = 0;
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 
 export async function GET() {
